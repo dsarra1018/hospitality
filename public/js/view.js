c5c0efda248070c854a6d60d69331bc6d1320c4e
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-05-01 22:08:47 -0400

    added view js, added body parser to server js

---
+++ public/js/view.js
@@ -0,0 +1,28 @@
+//
+$("#search-btn").on("click", function() {
+    const searchedCharacter = $("#name-search").val().trim();
+
+    
+    
+    searchedPatient = searchedPatient.replace(/\s+/g, "").toLowerCase();
+
+    $.get("/api/characters/" + searchedPatient, function(data) {
+      console.log(data);
+      if (data) {
+        $("#inputFirstName").text(data.firstName),
+        $("#inputLastName").text(data.lastName),
+        $("#inputDOB").text(data.dob),
+        $("#textareaSymptoms").text(data.symptoms),
+        $("#textareaDiagnosis").text(data.diagnosis),
+        $("#textareaTreatment").text(data.treatment)
+      } else {
+        $("#inputFirstName","#inputLastName").text(
+          "No patient found");
+      }
+    });
+});
+  // search for patient
+app.get('/search', (req, res){
+    const {term} = req.query;
+
+})
