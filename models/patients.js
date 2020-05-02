fd887fa7835748ad2373e9f55df4af16c796c35c
Regina Musovirova <musovirova@yahoo.com>
2020-05-02 11:23:36 -0400

    put links to logout

--- models/patients.js
+++ models/patients.js
@@ -1,26 +1,21 @@
-// Dependencies
-const DataTypes = require("sequelize");
-const sequelize = require("../config/connection");
-
-// Creates a "Patient" model.
-const Patient = sequelize.define("patients", {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+// Creating a "Patient" model
+module.exports = function(sequelize, DataTypes) {
+    const Patient = sequelize.define("patients", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        last_name: DataTypes.STRING,
+        first_name: DataTypes.STRING,
+        dob: DataTypes.STRING,
+        symptoms: DataTypes.TEXT,
+        diagnosis: DataTypes.TEXT,
+        treatment: DataTypes.TEXT
     },
-    last_name: DataTypes.STRING,
-    first_name: DataTypes.STRING,
-    dob: DataTypes.STRING, //Change to DATEONLY when schema.sql has been updated to reflect it.
-    symptoms: DataTypes.TEXT,
-    diagnosis: DataTypes.TEXT,
-    treatment: DataTypes.TEXT
-},
-{
-    freezeTableName: true,
-    timestamps: false
-});
-
-Patient.sync();
-
-module.exports = Patient;
+    {
+        freezeTableName: true,
+        timestamps: false
+    });
+    return Patient;
+}
