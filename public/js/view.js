51191792a22e0cfe5c93f734a77a6c805b4601aa
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-05-03 16:04:00 -0400

    view adjustments

--- public/js/view.js
+++ public/js/view.js
@@ -31,4 +31,56 @@ app.get('/search', (req, res){
     const {term} = req.query;
 
 })
+$(function() {
 
+  // Add a new burger.
+  $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+
+      var newBurger = {
+          burger_name: $("#newburger").val().trim(),
+          devoured: 0
+      };
+
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+      }).then(function() {
+          console.log("Added new burger");
+          // Reload the page to get the updated burger list.
+          location.reload();
+      });
+  });
+
+  $(".eatburger").on("click", function(event) {
+      event.preventDefault();
+
+      var id = $(this).data("id");
+      var devouredState = {
+          devoured: 1
+      };
+
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: devouredState
+      }).then(function() {
+          console.log("Burger devoured");
+          location.reload();
+      });
+  });
+
+  $(".trashburger").on("click", function(event) {
+      event.preventDefault();
+
+      var id = $(this).data("id");
+
+      // Send the DELETE request.
+      $.ajax({
+          type: "DELETE",
+          url: "/api/burgers/" + id
+      }).then(location.reload());
+  });
+
+})
