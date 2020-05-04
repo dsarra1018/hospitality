88a0a63d4398d5a7982cce95ebe5781fb2be6ed7
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-05-03 21:43:28 -0400

    fixed views js/html to display db

--- public/js/view.js
+++ public/js/view.js
@@ -1,12 +1,12 @@
 //get information from add form
 $("#search-btn").on("click", function() {
     $("patientRecord").toggle();
-    const searchedPatient= $("#name-search").val().trim();
+    let searchedPatient= $("#name").val().trim();
 
     searchedPatient = searchedPatient.replace(/\s+/g, "").toLowerCase();
 
-    $.get("/api/add" + searchedPatient, function(data) {
-      console.log(data);
+    $.get("/api/add", function(data) {
+      console.log("this is the data" + data);
       if (data) {
         $("#patientRecord").show();
         $("#inputFirstName").text(data.first_name),
@@ -22,63 +22,10 @@ $("#search-btn").on("click", function() {
       }
     });
 });
-  // search for patient
+  
+// search for patient
 
-app.get('/search', (req, res) => {
-
+app.get("/search", (req, res) => {
     const {term} = req.query;
-
-})
-$(function() {
-
-  // Add a new burger.
-  $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-
-      var newBurger = {
-          burger_name: $("#newburger").val().trim(),
-          devoured: 0
-      };
-
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-          type: "POST",
-          data: newBurger
-      }).then(function() {
-          console.log("Added new burger");
-          // Reload the page to get the updated burger list.
-          location.reload();
-      });
-  });
-
-  $(".eatburger").on("click", function(event) {
-      event.preventDefault();
-
-      var id = $(this).data("id");
-      var devouredState = {
-          devoured: 1
-      };
-
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-          type: "PUT",
-          data: devouredState
-      }).then(function() {
-          console.log("Burger devoured");
-          location.reload();
-      });
-  });
-
-  $(".trashburger").on("click", function(event) {
-      event.preventDefault();
-
-      var id = $(this).data("id");
-
-      // Send the DELETE request.
-      $.ajax({
-          type: "DELETE",
-          url: "/api/burgers/" + id
-      }).then(location.reload());
-  });
-
+    console.log(req.query);
 })
