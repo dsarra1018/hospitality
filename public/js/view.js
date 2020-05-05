1316d16563c13b07ca3abbf2a3495f2c57837a50
Darren Sarra <adriansarra18@gmail.com>
2020-05-05 12:24:09 -0400

    Added accordion collapse function on view.html file.

--- public/js/view.js
+++ public/js/view.js
@@ -24,4 +24,16 @@ $(document).ready(function() {
             }
         });
     });
+
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.show").each(function() {
+        $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+    });
+
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function() {
+        $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    }).on('hide.bs.collapse', function() {
+        $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });
 });
