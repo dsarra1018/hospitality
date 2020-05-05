385eeceff2b3c07e83fca3763d9d67de7f8033ac
Darren Sarra <adriansarra18@gmail.com>
2020-05-05 12:20:35 -0400

    Fixed search functionality.

--- public/js/view.js
+++ public/js/view.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $("#search-btn").on("click", function() {
         $("#patientRecord").toggle();
         let name = $("#name").val().trim();
-        let formatted_name = name.replace(/\s/g, "_").toLowerCase();
+        let formatted_name = name.replace(/\s/g, "_");
 
         $.get("/api/patient/name/" + formatted_name, function(data) {
 
@@ -12,17 +12,16 @@ $(document).ready(function() {
                 $("#patientRecord").show();
                 $("#inputFirstName").text(data.first_name),
                 $("#inputLastName").text(data.last_name),
-                console.log("Last Name: ", data.last_name),
                 $("#inputDOB").text(data.dob),
                 $("#textareaSymptoms").text(data.symptoms),
                 $("#textareaDiagnosis").text(data.diagnosis),
                 $("#textareaTreatment").text(data.treatment)
             } else {
-                $("#inputFistName","#inputLastName").text("No patient found");
-                $("#patientRecord").hide();
+                $("#patientRecord").show();
+                $("#result").text("No Patient Found");
+                $(".bs-example").hide();
+                $("#trashbutton").hide();
             }
         });
     });
-
-    // Delete information from database
 });
