5f0780c922864a4cd497f99be9e80b8577e93202
Regina Musovirova <musovirova@yahoo.com>
2020-05-06 17:37:20 -0400

    fixed js

--- public/js/api.js
+++ public/js/api.js
@@ -4,24 +4,26 @@ var new_death = document.getElementById("new_death");
 var total_death = document.getElementById("total_death");
 var total_recovered = document.getElementById("total_recovered");
 var total_cases = document.getElementById("total_cases");
-var table = document.getElementById('countries_stat')
 // Fetching the Data from the server
 
 //Fetching the World Data
 fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "ca23d0e808mshd2f630ae62cc87ep1afbd9jsn0afe9162f63a"
-    }
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+    "x-rapidapi-key": "ca23d0e808mshd2f630ae62cc87ep1afbd9jsn0afe9162f63a"
+  }
 })
-.then(function(response){response}.json().then(function(data) {
-    console.log(data);
-    total_cases.innerHTML = data.total_cases;
-    new_cases.innerHTML = data.new_cases;
-    new_death.innerHTML = data.new_deaths;
-    total_death.innerHTML = data.total_deaths;
-    total_recovered.innerHTML = data.total_recovered;
-}).catch(function(err) {
+  .then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+      total_cases.innerHTML = data.total_cases;
+      new_cases.innerHTML = data.new_cases;
+      new_death.innerHTML = data.new_deaths;
+      total_death.innerHTML = data.total_deaths;
+      total_recovered.innerHTML = data.total_recovered;
+    });
+  })
+  .catch(function(err) {
     console.log(err);
-});
+  });
